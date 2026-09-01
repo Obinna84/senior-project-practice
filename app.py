55fbe5b35f1d93aa9cb6f6b8e52bf5f5ffd75926
Obinna84 <68216674+Obinna84@users.noreply.github.com>
2026-09-01 13:45:39 -0400

    initialize README.md

--- app.py
+++
@@ -1,2 +0,0 @@
-DEVELOPER_PROFILE = "Senior Project Developer Profile\n\nName: Obinna Oramalu\nMajor: Computer Science\nTechnology Interest: Public Interest Technology\nSkill Goal: backend"
-print(DEVELOPER_PROFILE)
